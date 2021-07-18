fd9f038413479b9f410e022013bac5fccc4543d7
Liem Eldert <liemeldert@outlook.com>
2021-07-18 15:40:31 -0400

    slightly updated weverse db

--- CROWNBOT/database/weverse.py
+++ CROWNBOT/database/weverse.py
@@ -4,7 +4,8 @@
 # this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 from sqlalchemy import Column, String, Integer
 
-from tortoise import Tortoise, run_async
+from tortoise.models import Model
+from tortoise import fields
 
 from CROWNBOT.database.sessions import *
 
@@ -12,6 +13,12 @@ from CROWNBOT.database.sessions import *
 def save(data):
     session = make_sessions()
     save_session(data, session)
+
+
+class Tournament(Model):
+    guild = fields.IntField(pk=True)
+    communties = fields.TextField()
+    notify_channel = fields.IntField()
 
 
 class Weverse(Base):
